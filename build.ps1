36d5d47d1e9d9e8906c6a64bc93105a0e369d060
AGODA\jlaszlo <jeno.laszlo@agoda.com>
2018-07-23 15:55:40 +0700

    Added C# project + build.ps1

---
+++ build.ps1
@@ -0,0 +1,31 @@
+#$ErrorActionPreference = "Stop"
+
+$scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+
+cd $scriptDir
+
+$nodeVersion = node -v
+$npmVersion = npm -v
+$yarnVersion = yarn -v
+
+Write-Output "-------------------------------------------"
+Write-Output "Node: $nodeVersion, Npm: $npmVersion, Yarn: $yarnVersion"
+Write-Output "-------------------------------------------"
+Write-Output "Build project"
+yarn build
+Write-Output "-------------------------------------------"
+Write-Output "Running Generator"
+
+$schemaPath = Join-Path $scriptDir "samples/GitHub/input/GitHubSchema.json"
+$outPath = Join-Path $scriptDir "samples/GitHub/output/OutTest/GitHubSchema.cs"
+$queryPath = Join-Path  $scriptDir "samples/GitHub/input/*.graphql"
+$templatePath = Join-Path $scriptDir "dist"
+
+Write-Output "Schema Path: $schemaPath"
+Write-Output "Output Path: $outPath"
+Write-Output "Query Path: $queryPath"
+Write-Output "Template Path: $templatePath"
+
+yarn gql-gen --schema $schemaPath --template $templatePath --out $outPath $queryPath
+
+Write-Output "-------------------------------------------"
