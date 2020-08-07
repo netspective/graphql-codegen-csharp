0a43e4f70dddcb125f4c5fcb6d6ef00b1b4fb6c5
Kunjahamed-P <kunjahamed.p@netspective.support>
2020-08-07 10:12:02 +0530

    Correcting type generation
    Fix:
    *  Change type generation order
    *  Support more scalar types
    *  Fix type non nullable and graphtype issues
    *  Interface generation issue fixed

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,14 @@
 import * as index from './template.handlebars';
 import * as classes from './class.handlebars';
+import * as graphtype from './graphtype.handlebars';
 import * as interfaces from './interface.handlebars';
+import * as interfacegraphtype from './interfacegraphtype.handlebars';
 import * as schema from './schema.handlebars';
 import * as documents from './documents.handlebars';
 import * as selectionSet from './selection-set.handlebars';
 import * as fragments from './fragments.handlebars';
 import * as enumTemplate from './enum.handlebars';
+import * as enumGraphType from './enumgraphtype.handlebars';
 import { EInputType, GeneratorConfig } from 'graphql-codegen-core';
 import { getType } from './helpers/get-type';
 import { getOptionals } from './helpers/get-optionals';
@@ -15,12 +18,15 @@ export const config: GeneratorConfig = {
   templates: {
     index,
     classes,
+    graphtype,
     interfaces,
+    interfacegraphtype,
     schema,
     documents,
     selectionSet,
     fragments,
-    enum: enumTemplate
+    enum: enumTemplate,
+    enumGraphType
   },
   flattenTypes: true,
   primitives: {
